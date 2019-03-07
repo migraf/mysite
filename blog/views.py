d775f0065674341ffb1a3fb7f1e965b8a4111fad
Michael Graf <michael.m.graf@gmx.de>
2019-03-07 11:16:10 +0100

    base layout navbar and basic links to blog

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class Index(TemplateView):
+    template_name = "index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["test"] = "hello"
+        return context
+
+
+class BasicBlogView(TemplateView):
+    template_name = "blog/base_blog.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["test"] = "hello"
+        return context
